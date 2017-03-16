dec066b73b12539b56d059366c536645af545a8d
Robert Hamilton <rghamilton3@gmail.com>
2017-03-15 20:17:31 -0500

    Add template styling to manage.php

--- queue/manage.php
+++ queue/manage.php
@@ -1,4 +1,12 @@
-
+<?php
+/*
+ *   CC BY-NC-AS UTA FabLab 2016-2017
+ *   FabApp V 0.9
+ */
+include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/header.php');
+$device_array = array();
+$_SESSION['type'] = "home";
+?>
 <?php
     
     
@@ -46,7 +54,7 @@ return $time_stamp;
     
 function get_q_count(){
     
-        $ctr==0;
+        $ctr = 0;
         $conn = get_connection();
 // Check connection
 if ($conn->connect_error) {
@@ -76,7 +84,7 @@ return $ctr;
     
  function get_q_all(){
     
-        $ctr==0;
+        $ctr = 0;
         $conn = get_connection();
 // Check connection
 if ($conn->connect_error) {
@@ -114,50 +122,46 @@ return $ctr;
 
 ?> 
 
+<title><?php echo $sv['site_name'];?> Waitlist Management</title>
 
-<!DOCTYPE html>
-<html>
-    <h2> Waitlist Admin Page (Under Construction) </h2>
-<head>
-    <title>Show Waitlist</title>
-<style>
-  
-table, th, td {
-    border: 5px solid black;
-    border-collapse: collapse;
-    text-align:center; 
-    vertical-align:middle;
-}
-th, td {
-    padding: 15px;
-}
-h2{
-    text-align:center; 
-}
-</style>
-</head>
-<body>
-    
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Manage Queue</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-ticket fa-fw"></i> Queue Status
+                </div>
+                <div class="panel-body">
+                    <table class="table table-striped table-bordered table-hover">
+                        <tr class="tablerow">
+                            <th>Number</th>
+                            <th>UTAID</th> 
+                            <th>Signed In</th>
+                            <th>Expected Wait</th>
+                        </tr>
+ 
+                        <?php
 
-<table style="width:80%">
-  <tr>
-    <th>Number</th>
-    <th>UTAID</th> 
-    <th>Signed In</th>
-    <th>Expected Wait</th>
-  </tr>
- 
-  <?php
- 
- get_q_all();
-  
-  ?>
- 
- 
- 
-</table>
+                        get_q_all();
+                        ?>
 
-
-
-</body>
-</html>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- /.col-lg-8 -->
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+<?php
+//Standard call for dependencies
+include_once ($_SERVER['DOCUMENT_ROOT'].'/pages/footer.php');
+?>
