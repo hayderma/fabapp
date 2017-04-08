859409cd2c5582c6992783b76f61c1653095b0cd
Haider MA <hayderma@users.noreply.github.com>
2017-04-08 14:12:05 -0500

    chabged

--- queue/connecting.php
+++ queue/connecting.php
@@ -1,6 +1,7 @@
 <?php 
 	class connecting {
 		var $name;
+            
                 function get_connection(){
                     $servername = "localhost";
                     $username = "root";
@@ -32,6 +33,38 @@
                 if($row = $result->fetch_assoc()) {
                 $id_num=$row["UID"];
                 if(strcmp($id_num, $UTAID)==0){
+                    $res=true;
+                }
+             }
+        } 
+     
+        $conn->close();
+        
+        return $res;
+    }
+    
+    
+    
+         function ticket_exists_already($ticket_number){
+    
+                    
+                    
+                     $conn = $this->get_connection();
+                     $res=false;
+                    // Check connection
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            return;
+            }
+
+            $sql = "select ticket_num FROM queue WHERE ticket_num ='".$ticket_number."'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                if($row = $result->fetch_assoc()) {
+                $t_num=$row["ticket_num"];
+                if(strcmp($t_num, $ticket_number)==0){
                     $res=true;
                 }
              }
@@ -121,6 +154,83 @@
         
         
     }   
+    
+    
+        function material_selector(){
+    
+                    
+                    
+                     $conn = $this->get_connection();
+                     $res=false;
+                    // Check connection
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            return;
+            }
+
+            
+            $sql = "SELECT m_name AS name FROM materials; ";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output machine names to selector in HTML
+                echo '<select name="material_select">'; 
+  
+
+                while($row = $result->fetch_assoc()) {
+                    $name =$row["name"];
+                   $one_option='<option value='.$name.'>'.$name.'</option>'; 
+                   echo $one_option;
+               
+                
+             }
+             echo '</select>'; 
+        } 
+     
+        $conn->close();
+        
+        
+    }  
+    
+     function color_selector($material){
+    
+                    
+                    
+                     $conn = $this->get_connection();
+                     $res=false;
+                    // Check connection
+                if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            return;
+            }
+
+            
+            $sql = "SELECT color_hex AS color FROM materials WHERE m_name=".$material." ; ";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output machine names to selector in HTML
+                echo '<select name="machine_select">'; 
+  
+
+                while($row = $result->fetch_assoc()) {
+                   
+                   $color = $row["color"];
+                   if ($color == NULL){
+                       $color ='No Color Defined';
+                   }
+                   $one_option='<option value='.$color.'>'.$color.'</option>'; 
+                   echo $one_option;
+               
+                
+             }
+             echo '</select>'; 
+        } 
+     
+        $conn->close();
+        
+        
+    }
 		
           
           
